39c80f274abe2bf638dfb23f7e9722be4ff033ee
Anthony01M <anthony19990maalouf@gmail.com>
2024-12-31 13:27:07 +0000

    chore: Tab -> Space

--- commands/echo.js
+++ commands/echo.js
@@ -3,24 +3,24 @@ const { SlashCommandBuilder } = require('discord.js')
 const MessageHelper = require('../helper/messageHelper')
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('echo')
-        .setDescription('Replies with your input.')
-        .addStringOption(option => option
-            .setName('input')
-            .setDescription('The input to echo back.')
-            .setMaxLength(500)
-            .setRequired(true)
-        ),
-    async execute(interaction) {
-        const inputMessage = interaction.options.getString('input', true),
-            sanitizedMessage = inputMessage.replace(/[`@#*]/g, '\\$&'),
-            reversedMessage = sanitizedMessage.split('').reverse().join(''),
-            messageHelper = new MessageHelper('ECHO'),
-            response = messageHelper.build({
-                original: sanitizedMessage,
-                reversed: reversedMessage
-            })
-        await interaction.reply(response)
-    }
+	data: new SlashCommandBuilder()
+		.setName('echo')
+		.setDescription('Replies with your input.')
+		.addStringOption(option => option
+			.setName('input')
+			.setDescription('The input to echo back.')
+			.setMaxLength(500)
+			.setRequired(true)
+		),
+	async execute(interaction) {
+		const inputMessage = interaction.options.getString('input', true),
+			sanitizedMessage = inputMessage.replace(/[`@#*]/g, '\\$&'),
+			reversedMessage = sanitizedMessage.split('').reverse().join(''),
+			messageHelper = new MessageHelper('ECHO'),
+			response = messageHelper.build({
+				original: sanitizedMessage,
+				reversed: reversedMessage
+			})
+		await interaction.reply(response)
+	}
 }
